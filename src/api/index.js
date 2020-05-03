59f47afc36a33edcbf8b6b729e7f9e23063cb8dd
Reshaeala <58646198+Reshaeala@users.noreply.github.com>
2020-05-03 16:40:18 -0500

    adding data to app.js

--- src/api/index.js
+++ src/api/index.js
@@ -4,11 +4,11 @@ const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async () => {
     try{
-        const response = await axios.get(url);
-        console.log(response);
-        return response;
-    }
-    catch(error){
+        const { data: { confirmed, recovered, deaths, lastUpdate} } = await axios.get(url);
+        
+        const modifiedData = {confirmed, recovered, deaths, lastUpdate }
+        return modifiedData;
+    }catch(error){
 
     }
 }
